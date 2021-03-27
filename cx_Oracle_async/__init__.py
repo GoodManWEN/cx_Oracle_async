5042f4ee59c9e397cc17f3e3fcc6d66db052aa41
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-03-27 11:51:38 +0000

    Update __init__.py

--- cx_Oracle_async/__init__.py
+++ cx_Oracle_async/__init__.py
@@ -3,8 +3,8 @@ __version__ = ''
 from .utils import create_pool , makedsn , DEQ_NO_WAIT , DEQ_WAIT_FOREVER
 
 __all__ = (
-	'create_pool',
-	'makedsn',
+    'create_pool',
+    'makedsn',
     'DEQ_NO_WAIT',
     'DEQ_WAIT_FOREVER'
 )
